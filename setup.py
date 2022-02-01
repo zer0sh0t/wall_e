186028efd8ef25f101425d1735209af8771226da
kumar-harin <haryn.k10@gmail.com>
2022-02-01 23:51:00 +0530

    adding setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='wall_e',
+    version='0.0.0',
+    author='zer0sh0t',
+    packages=find_packages(),
+    install_requires=['sympy', 'scipy']
+)
