9a169848fa8a49297b1f7e0bcfb834a06dea0f00
kumar-harin <haryn.k10@gmail.com>
2022-02-22 19:41:01 +0530

    updating example

--- example.py
+++ example.py
@@ -1,6 +1,8 @@
 import math
 import wall_e
 
+name = 'double_link_planar_robot'
+type_ = 'rr' # 2 rotary joints
 dh_params = [
      [math.pi/2, 0, 0, 20], # theta, d, alpha, a
      [math.pi, 0, 0, 10],
@@ -13,21 +15,30 @@ vels = [10, 5]
 accs = [0, 0]
 taus_req = [2207, 343]
 
-bender = wall_e.Robot('bender', 'rr', dh_params, masses, lengths, dimensions) # 'rr' - 2 rotary joints
-print(bender)
+r = wall_e.Robot(name, type_, dh_params, masses, lengths, dimensions)
+print(r)
+
+# render the robot
+r.render()
+ 
+# move the robot to the given position end position and render it
+r.move(end_pos=end_pos) # you can also provide the final_angles instead of end_pos
+ 
+# plot the trajectory for moving the robot to the given end position
+r.move_traj(10, end_pos=end_pos) # you can also provide the final_angles instead of end_pos
 
 # forward kinematics
-fk_mat_expr, fk_mat = bender.solve_fk()
+fk_mat_expr, fk_mat = r.solve_fk()
 wall_e.pprint(fk_mat_expr); print();
 wall_e.pprint(fk_mat)
 
 # inverse kinematics
-thetas, fk_mat = bender.solve_ik(end_pos)
+thetas, fk_mat = r.solve_ik(end_pos)
 print(thetas, end='\n'*2)
 wall_e.pprint(fk_mat)
 
 # inverse dynamics
-tau_exprs, taus = bender.solve_id(thetas, vels, accs)
+tau_exprs, taus = r.solve_id(thetas, vels, accs)
 for tau_expr, tau in zip(tau_exprs, taus):
     print('tau_expr:')
     wall_e.pprint(tau_expr)
@@ -39,10 +50,10 @@ computation of forward dynamics takes a lot of time, i suggest you to create a n
 override the solve_id() method and rewrite the output tau expressions in numpy like shown below 
 as sympy is super slow at numeric computation
 
-class Bender(Robot):
+class NewRobotClass(Robot):
     def solve_id():
         # write the inverse dynamics logic here in numpy
 '''
-optim_vals, taus = bender.solve_fd(taus_req)
+optim_vals, taus = r.solve_fd(taus_req)
 print(optim_vals)
 print(taus)
